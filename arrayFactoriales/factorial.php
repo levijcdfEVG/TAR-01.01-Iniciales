4f14291f18ec8f4d0dfb3ed185cee5eb7568643b
levijcdfEVG <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-09-23 20:12:54 +0100

    reestructuracion del codigo

--- arrayFactoriales/factorial.php
+++ arrayFactoriales/factorial.php
@@ -9,29 +9,14 @@
         <link href="../css/form.css" rel="stylesheet" />
     </head>
     <body>
-        <h1>Tabla de Factoriales</h1>
-        <div class="tableContainer">
-            <table>
-                <tr>
-                    <?php
-                      require 'algoritmo.php';
-                      echo '<th colspan="2" id="tableTitle">TABLA DE FACTORIALES DEL 0 AL 10</th>';
-                    ?>  
-                </tr>
-                <tr>
-                    <td>NÚMERO</td>
-                    <td>FACTORIAL</td>
-                </tr>
-                    <?php
-                         // Imprimir el array para comprobar los resultados
-                        for ($i=0; $i < 9; $i++) { 
-                            echo "$arrayPruebas[$i] <br>";
-                        }
-                    ?>
-                <tr>
-                    <td colspan="2">Levi Josué Candeias de Figueiredo</td>
-                </tr>
-            </table>
+        <h1>Tabla de Factoriales del 0 al 10</h1>
+        <?php
+            require 'algoritmo.php';
+             // Imprimir el array para comprobar los resultados
+            for ($i=0; $i < 9; $i++) { 
+                echo "$arrayPruebas[$i] <br>";
+            }
+        ?>
         </div>
     </body>
 </html>
