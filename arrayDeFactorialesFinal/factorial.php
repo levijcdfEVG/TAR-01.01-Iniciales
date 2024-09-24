d7a35eb694deb9183174d9929c01b4e48bdedbda
levijcdfEVG <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-09-24 10:41:51 +0200

    Actualizar Repositorio para terminar en casa

--- arrayDeFactorialesFinal/factorial.php
+++ arrayDeFactorialesFinal/factorial.php
@@ -12,15 +12,10 @@
         <h1>Tabla de Factoriales</h1>
         <table>
         <?php
-            require 'algoritmo2.php';
-            //Inicializa las variables
-           
+            require 'algoritmo.php';
+
             
-            for($i = 0; $i <= count($arrayPruebas)-1; $i++) { 
-               echo "<tr><td>$i</td><td>$arrayPruebas[$i]</td></tr>";
-            }
-            // print_r($arrayPruebas); Esto sirve para ver todos los valores del array y no se suele utilzar en las aplicaciones que se van a utilizar por los usuarios
-        ?>
+        ?>  
         </table>    
         </div>
     </body>
---
+++ arrayFactoriales/prueba.php
@@ -0,0 +1,7 @@
+<?php 
+    require 'algoritmo2.php';
+
+    $factorial = calcularFactorial(0);
+
+    echo "$factorial";
+?>
