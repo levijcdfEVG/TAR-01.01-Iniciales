51adef3d44867bf114c6fa21c2930196f2744407
levijcdfEVG <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-09-24 09:32:37 +0200

    Mover los archivos y cambiar las rutas relativas

--- ejercicio1/indexVer2.php
+++ ejercicio1/indexVer2.php
@@ -4,9 +4,9 @@
     <head>
         <title>Ejercicios Iniciales PHP</title>
         <meta charset="utf-8" />
-        <link href="css/reset.css" rel="stylesheet" />
-        <link href="css/table.css" rel="stylesheet" />
-        <link href="css/mainPage.css" rel="stylesheet" />
+        <link href="../css/reset.css" rel="stylesheet" />
+        <link href="../css/table.css" rel="stylesheet" />
+        <link href="../css/mainPage.css" rel="stylesheet" />
     </head>
     <body>
         <h1>Tabla de Factoriales</h1>
