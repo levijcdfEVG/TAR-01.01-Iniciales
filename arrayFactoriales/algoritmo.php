d67959dc0f9494fd1a77527c41dac923e2073392
levijcdfEVG <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-09-24 09:45:34 +0200

    Apuntes sobre la visualización de arrays

--- arrayFactoriales/algoritmo.php
+++ arrayFactoriales/algoritmo.php
@@ -1,6 +1,6 @@
 <?php 
     $arrayPruebas = array(); // Inicializar el array vacío
-
+    
     // Recorremos del 0 al 10 para calcular los factoriales
     for ($i = 0; $i <= 10; $i++) { 
         $factorial = 1; // Inicializamos el factorial en 1 para cada número
--- arrayFactoriales/factorial.php
+++ arrayFactoriales/factorial.php
@@ -13,10 +13,12 @@
         <table>
         <?php
             require 'algoritmo.php';
+            // echo $$arrayPruebas
              // Imprimir el array para comprobar los resultados
             for ($i=0; $i < 9; $i++) { 
-                echo "<tr><td>$i</td><td>$arrayPruebas[$i]</td></tr>";
+               echo "<tr><td>$i</td><td>$arrayPruebas[$i]</td></tr>";
             }
+            // print_r($arrayPruebas); Esto sirve para ver todos los valores del array y no se suele utilzar en las aplicaciones que se van a utilizar por los usuarios
         ?>
         </table>    
         </div>
